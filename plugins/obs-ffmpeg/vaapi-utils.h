bd7420d49aa9f32e59a328f902ef0f07223a4c99
David Rosca <nowrep@gmail.com>
2022-12-21 16:21:46 +0100

    obs-ffmpeg: Add preset and pre-encode options for VAAPI
    v2: High Quality preset for AV1

--- plugins/obs-ffmpeg/vaapi-utils.h
+++ plugins/obs-ffmpeg/vaapi-utils.h
@@ -14,6 +14,8 @@ void vaapi_close_device(int *fd, VADisplay dpy);
 bool vaapi_device_rc_supported(VAProfile profile, VADisplay dpy, uint32_t rc, const char *device_path);
 bool vaapi_device_bframe_supported(VAProfile profile, VADisplay dpy);
 
+bool vaapi_quality_preset_supported(VAProfile profile, VADisplay dpy);
+
 bool vaapi_display_h264_supported(VADisplay dpy, const char *device_path);
 bool vaapi_device_h264_supported(const char *device_path);
 const char *vaapi_get_h264_default_device(void);
